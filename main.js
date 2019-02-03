2804d05186b16e1ab5d22ac14b0763d746cb5957
daniel <bejtrup@gmail.com>
2019-02-03 14:46:50 +0100

    fix event change bug

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ function checkDbNeedUpdate(db_id, db_url){
     var eventLastUpdate = eventUpdateLog.find(b=>b.eventId == db_id).lastUpdate;
     var moreThan24h = nowArray[0] > eventLastUpdate[0] && nowArray[1] >= eventLastUpdate[1] && nowArray[2] >= eventLastUpdate[2] && nowArray[3] > eventLastUpdate[3];
     
-    if(moreThan24h){
+    if ( !localStorage.getItem("bandsFromStorrage") || JSON.parse(localStorage.getItem("bandsFromStorrage"))[parseInt(db_id)] == undefined || moreThan24h) {
         console.log("time for update bands[]");
         getDB(db_url);
         eventUpdateLog.find(b=>b.eventId == db_id).lastUpdate = nowArray;
